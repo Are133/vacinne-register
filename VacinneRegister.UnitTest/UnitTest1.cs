b6d186c1bc7b604533cc31ae6c909e2b7a4304b1
DESKTOP-UOTJFFC\Isaid <iarena@ia.com.mx>
2022-06-02 16:29:51 -0500

    Feat(Arquitectura): Arquitectura Inicial
    Agrega la Arquitectura Inicial del proyecto API.

---
+++ VacinneRegister.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace VacinneRegister.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
