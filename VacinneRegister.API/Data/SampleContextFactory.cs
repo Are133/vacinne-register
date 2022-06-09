e1222599682578354c6edb96d467f081c55cbdb9
DESKTOP-UOTJFFC\Isaid <iarena@ia.com.mx>
2022-06-09 02:22:40 -0500

    Fix(Migration): Migrations Error
    Add Class to Project API For Corrected Migrations in the Application

---
+++ VacinneRegister.API/Data/SampleContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using VacinnerRegister.Infraestructure.Data;
+
+namespace VacinneRegister.API.Data
+{
+    public class SampleContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DataContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString, b => b.MigrationsAssembly("VacinneRegister.API"));
+
+            return new DataContext(builder.Options);
+        }
+    }
+}
